675c9e269093f198b42ad1d8fa1ba131b73584d1
Jagteshver Singh Sahni <85994817+JS-CodeGamer@users.noreply.github.com>
2022-03-30 02:07:25 +0530

    .

--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from DashboardApp.interconnect import send_request_get
+from numerize import numerize
 import json
 
 
@@ -13,10 +14,18 @@ def dashboard(request):
     users_url = "http://userapp.centralindia.cloudapp.azure.com:8080/api/usershistory"
     [_, users_response] = send_request_get(users_url)
     users_data = users_response.json()
-    user_months = list(users_data.keys())
+    user_months = list(users_data["history"].keys())
     user_months.reverse()
-    users = [users_data[i] for i in user_months]
-    return render(request, 'dashboard.html', {'sales_months': sales_months,
-                                                'sales': sales,
-                                                'user_months': user_months,
-                                                'users': users })
+    users = [users_data["history"][i] for i in user_months]
+    context = {
+        'sales_months': sales_months,
+        'sales': sales,
+        'user_months': user_months,
+        'users': users,
+        "user_increase_percent": (users[-1]-users[-2])/100,
+        "sales_increase": (sales[-1]-sales[-2]),
+        "customers": numerize.numerize(users_data["customer"]),
+        "staff": numerize.numerize(users_data["staff"]),
+        "delivery": numerize.numerize(users_data["delivery"])
+        }
+    return render(request, 'dashboard.html', context)
